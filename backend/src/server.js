6fda93d38d34e17c5b8fd1f0211b92cb2561baf2
Charles Eduardo <charleseduardo.me@gmail.com>
2019-10-08 18:50:38 -0300

    chore: Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const socketio = require('socket.io');
+const http = require('http');
+
+const routes = require('./routes');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+mongoose.connect(
+  'mongodb+srv://omnistack:omnistack@omnistack-uigjz.mongodb.net/semana09?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+// GET (BUSCAR), POST (INSERIR), PUT (EDITAR), DELETE (DELETAR)
+
+// req.query = Acessar query params (para filtrar)
+// req.params = Acessar router params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+server.listen(3333);
